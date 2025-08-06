094cf2a91b72b9cbd4fe8f7410789baf4380a85e
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-06 18:55:08 +0530

    22167

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.conf import setttings
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'restaurant_name':setttings.RESTAURANT_NAME})
