a3fcf584753aa13cbe73613e8ab46b1e10800fc3
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-07 16:34:52 +0530

    22171

--- home/views.py
+++ home/views.py
@@ -7,3 +7,6 @@ def home(request):
         'restaurant_name':settings.RESTAURANT_NAME
     }
     return render(request, 'home.html', context)
+
+def contact(request):
+    return render(request, 'contact.html')
