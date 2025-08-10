cf2dcee529040238dc3800d88558cd59baa749f4
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-10 16:49:01 +0530

    28846

--- home/views.py
+++ home/views.py
@@ -4,13 +4,15 @@ from django.conf import settings
 def home(request):
     context = {
         'restaurant_name':settings.RESTAURANT_NAME,
-        'restaurant_phone': settings.RESTAURANT_PHONE_NUMBER
+        'restaurant_phone': settings.RESTAURANT_PHONE_NUMBER,
+        'current_year':now().year
     }
     return render(request, 'home.html', context)
 
 def contact(request):
     context = {
-        'restaurant_phone':settings.RESTAURANT_PHONE
+        'restaurant_phone':settings.RESTAURANT_PHONE,
+        'current_year':now().year
     }
     return render(request, 'contact.html', context)
 
