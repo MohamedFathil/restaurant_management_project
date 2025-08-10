c91bb14462371d7ebe816121e44c1c8cf7ad1190
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-10 21:04:13 +0530

    28849

--- home/views.py
+++ home/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
 
 def home(request):
-    context = {
-        'restaurant_name':settings.RESTAURANT_NAME,
-        'restaurant_phone': settings.RESTAURANT_PHONE_NUMBER,
-        'current_year':now().year
-    }
-    return render(request, 'home.html', context)
+    try:
+        context = {
+            'restaurant_name':settings.RESTAURANT_NAME,
+            'restaurant_phone': settings.RESTAURANT_PHONE_NUMBER,
+            'current_year':now().year
+        }
+        return render(request, 'home.html', context)
+    except Exception as e:
+        print(f"Error in home view : {e}")
+        return HttpResponse("Oops! Something went wrong while loading the home page.", status=500)
 
 def contact(request):
-    context = {
-        'restaurant_phone':settings.RESTAURANT_PHONE,
-        'current_year':now().year
-    }
-    return render(request, 'contact.html', context)
+    try:
+        context = {
+            'restaurant_phone':settings.RESTAURANT_PHONE,
+            'current_year':now().year
+        }
+        return render(request, 'contact.html', context)
+    except Exception as e:
+        print(f"Error im contact view : {e}")
+        return HttpResponse("Oops! Something went wrong while loading the contact page.",status=500)
 
 def reservations(request):
     """Render the reservation page"""
