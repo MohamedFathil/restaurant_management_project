e3b085132fe5de199a510a99cc887ef18e5cc0f2
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-13 09:34:21 +0530

    29144

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('reservations/',views.reservations, name='reservations'),
+    path('feedback/', views.feedback_view, name='feedback'),
 ]
