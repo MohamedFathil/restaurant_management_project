02ea1d242b329c0d751f5718c35c521235b87ad6
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-13 10:54:32 +0530

    31692

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Order
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer','total_amount','order_status','created_at']
+    list_filter = ['order_status','created_at']
+    search_field = ['customer__username']
+    filter_horizontal = ('order_items',)
+
+admin.site.register(Order, OrderAdmin)
 
