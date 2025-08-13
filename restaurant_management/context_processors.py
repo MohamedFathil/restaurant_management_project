6c1332107529b5f392387871711b85ee82287e1a
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-13 09:49:38 +0530

    29145

--- restaurant_management/context_processors.py
+++ restaurant_management/context_processors.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-def restaurant_logo(request):
+def restaurant_info(request):
     return {
         "restaurant_name":getattr(settings, "RESTAURANT_NAME", "Our Restaurant"),
         "restaurant_opening_hours": getattr(settings, "RESTAURANT_OPENING_HOURS", ""),
