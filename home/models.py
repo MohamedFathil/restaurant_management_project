7f4f6f83f684aac3db89589d7ce4abe8e0e76b4f
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-13 09:27:28 +0530

    29144

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    comment = models.TextField()
+    submitted_at = models.DateTimeField(auto_add_now = True)
+
+    def __str__(self):
+        return f"Feedback #{self.id} - {self.comment[:20]}"
