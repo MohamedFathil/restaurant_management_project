ffb577245a2e7b07e56461262e3463765c21da2a
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-13 10:41:35 +0530

    31691

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import  MenuItem
 
-# Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    order_items = models.ManyToMany(MenuItem, related_name="orders")
+    total_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    order_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_add_now=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+        
