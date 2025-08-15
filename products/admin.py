ea56e5e1d2443daaf1b57a7827fced81dd22cc78
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-15 18:32:54 +0530

    31726

--- products/admin.py
+++ products/admin.py
@@ -4,8 +4,8 @@ from .models import MenuItem
 
 # Custom Admins
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
-    search_field = ['item_name']
+    list_display = ['name','description','price','created_at']
+    search_field = ['name']
 
 # Register your models here.
 admin.site.register(MenuItem,MenuItemAdmin)
