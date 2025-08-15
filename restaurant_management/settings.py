6b831bf0d51d55e638f52aa4ec5b61c325e2a328
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-15 18:21:20 +0530

    31728

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-RESTAURANT_NAME = "Bite a Bite"
+RESTAURANT_NAME = "Bite the Bite"
 RESTAURANT_PHONE_NUMBER = "+91 9876543210"
 RESTAURANT_OPENING_HOURS = "Mon-Fri: 11am - 9pm, Sat-Sun: 10am - 10pm"
 SITE_URL = "http://localhost:8000"
