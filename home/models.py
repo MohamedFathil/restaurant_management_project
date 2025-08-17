fd506d4a5ccecf252d3ec1cba0edf92ebf9da21a
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-17 18:17:56 +0530

    34564

--- home/models.py
+++ home/models.py
@@ -14,3 +14,11 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
