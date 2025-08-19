68da0d78edab0cd76ec93640d5913e0e32cd46f2
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-19 18:22:27 +0530

    34571

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from home.views import contact
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
