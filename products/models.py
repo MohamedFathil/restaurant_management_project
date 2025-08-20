fc837d2a6229385345f424268165e619b776fe5e
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-20 11:13:46 +0530

    34566

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to="menu_images", blank=True, null=True)
+    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
