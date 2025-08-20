4c24c217340705655afbc17ebb6dcab58cd23921
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-20 11:30:52 +0530

    36353

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Contact, RestaurantAddress, Restaurant
 
+# registering models
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name','email','created_at')
