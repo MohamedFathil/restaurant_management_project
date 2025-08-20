2fcaad26c21940ffc5865da7aa3f4ce7898dfe14
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-20 11:12:19 +0530

    34566

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ItemView, MenuItemView, menu_page
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-api'),
-    path('menu/', MenuItemView.as_view(), name='menu-api'),
+    path('api/items/', ItemView.as_view(), name='item-api'),
+    path('api/menu/', MenuItemView.as_view(), name='menu-api'),
     # template page
     path('menu-page/', menu_page, name='menu-page'),
 ]
