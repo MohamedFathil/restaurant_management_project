d30aaebb587f5330fa00f48a73406a42a6337518
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-21 11:33:55 +0530

    36351

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('reservations/',views.reservations, name='reservations'),
     path('contact/', views.contact, name='contact'),
+    path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('feedback/', views.feedback_view, name='feedback'),
     path('faq/', views.faq_view, name='faq'),
     path('about/', views.about_view, name='about'),
