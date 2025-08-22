8372d39471452dca01d8410303f49a398416f366
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-22 16:29:04 +0530

    36892

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     path('products/', include('products.urls')),
-    path('contact/', contact, name='contact'),
-    path('orders/', include('orders.urls'))
+    path('orders/', include('orders.urls')),
     path('api/',include('home.urls')),
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
