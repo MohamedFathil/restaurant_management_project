267762bc9f85f799010f927aae17cbd5d2668d7a
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-22 17:33:01 +0530

    36893

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import auth_views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('reservations/',views.reservations, name='reservations'),
     path('contact/', views.contact, name='contact'),
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
@@ -12,5 +13,4 @@ urlpatterns = [
     path('faq/', views.faq_view, name='faq'),
     path('about/', views.about_view, name='about'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
-    
 ]
