c1a3e21a0fdfdc82b1655030440ae819c69db2a4
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-22 22:11:09 +0530

    36895

--- orders/views.py
+++ orders/views.py
@@ -6,3 +6,12 @@ def order_page(request):
         {'name':'Order', 'url':'/order/'}
     ]
     return render(request, 'order.html',{'breadcrumb':breadcrumb})
+
+def order_confirmation(request):
+    breadcrumb = [
+        {'name':'Home', 'url':'/'},
+        {'name':'Menu', 'url':'/menu/'},
+        {'name':'Order', 'url':'/order/'},
+        {'name':'Confirmation', 'url':''}
+    ]
+    return render(request, 'order_confirmation.html', {'breadcrumb':breadcrumb})
