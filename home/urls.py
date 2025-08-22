7abc0fc1bd71b1c02b254717b578fbc0e76bfb46
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-22 16:45:09 +0530

    36893

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('reservations/',views.reservations, name='reservations'),
     path('contact/', views.contact, name='contact'),
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
