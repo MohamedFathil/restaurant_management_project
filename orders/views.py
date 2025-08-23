7bbe1933f726ce2569eedf1a791b4ffad060b19a
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-23 10:04:41 +0530

    36894

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 def order_page(request):
     breadcrumb = [
@@ -8,10 +9,11 @@ def order_page(request):
     return render(request, 'order.html',{'breadcrumb':breadcrumb})
 
 def order_confirmation(request):
+    order_id = random.randint(1000, 9999)
     breadcrumb = [
         {'name':'Home', 'url':'/'},
         {'name':'Menu', 'url':'/menu/'},
         {'name':'Order', 'url':'/order/'},
         {'name':'Confirmation', 'url':''}
     ]
-    return render(request, 'order_confirmation.html', {'breadcrumb':breadcrumb})
+    return render(request, 'order_confirmation.html', {'breadcrumb':breadcrumb, 'order_id':order_id})
