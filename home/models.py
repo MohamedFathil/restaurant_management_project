177739134ff3dc005c43cab0762e92ad81927732
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-23 12:06:34 +0530

    38079

--- home/models.py
+++ home/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class Feedback(models.Model):
-    comment = models.TextField()
+    name = models.CharField(max_length=100)
+    feedback = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return f"Feedback #{self.id} - {self.comment[:20]}"
+        return f"Feedback #{self.name} - {self.submitted_at.strftime('%Y-%m-%d)}"
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
