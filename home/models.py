62e5c3e6d47404db539966115e1df8906027928c
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-23 14:17:43 +0530

    38076

--- home/models.py
+++ home/models.py
@@ -43,3 +43,12 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class TodaysSpecial(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
