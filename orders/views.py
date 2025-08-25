d39abf9872be21c5921d813800f81a70f71c038e
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-25 10:32:38 +0530

    38077

--- orders/views.py
+++ orders/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render, redirect
 import random
 
 
-def add_to_cart(request, product_id):
+def add_to_cart(request, item_id):
+    """Add item to the cart"""
     cart = request.session.get('cart',{})
     cart[item_id] = cart.get(item_id, 0) + 1
     request.session['cart'] = cart
     return redirect('view_cart')
 
+def view_cart(request):
+    """View cart page"""
+    cart = request.session.get('cart',{})
+    breadcrumb = [
+        {'name':'Home','url':'/'},
+        {'name':'Cart', 'url':'/cart/'}
+    ]
+    return render(request, 'cart.html', {'breadcrumb':breadcrumb, 'cart':cart})
+
+def remove_from_cart(request, item_id):
+    """Remove item from the cart"""
+    cart = request.session.get('cart',{})
+    if item_id in cart:
+        def cart[item_id]
+    request.session['cart'] = cart
+    return redirect('view_cart')
 
 def order_confirmation(request):
     order_id = random.randint(1000, 9999)
@@ -17,4 +34,5 @@ def order_confirmation(request):
         {'name':'Order', 'url':'/order/'},
         {'name':'Confirmation', 'url':''}
     ]
+    request.session['cart'] = {}
     return render(request, 'order_confirmation.html', {'breadcrumb':breadcrumb, 'order_id':order_id})
