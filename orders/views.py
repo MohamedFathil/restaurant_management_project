cdf4d7f96de69f660307d94a099e0275a734024f
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-25 14:55:31 +0530

    38077

--- orders/views.py
+++ orders/views.py
@@ -1,22 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import MenuItem
 import random
 
 
 def add_to_cart(request, item_id):
     """Add item to the cart"""
+    item = get_object_or_404(MenuItem, id=item_id)
     cart = request.session.get('cart',{})
-    cart[item_id] = cart.get(item_id, 0) + 1
+    if str(item_id) in cart:
+        cart[str(item_id)]['quantity'] += 1
+    else:
+        cart[str(item_id)] = {
+            'name':item.name,
+            'price': float(item.price),
+            'quantity':1
+        }
     request.session['cart'] = cart
     return redirect('view_cart')
 
 def view_cart(request):
     """View cart page"""
     cart = request.session.get('cart',{})
+    total_price = sum(item['price']* item['quantity'] for item in cart.values())
     breadcrumb = [
         {'name':'Home','url':'/'},
         {'name':'Cart', 'url':'/cart/'}
     ]
-    return render(request, 'cart.html', {'breadcrumb':breadcrumb, 'cart':cart})
+    return render(request, 'cart.html', {'breadcrumb':breadcrumb, 'cart':cart. 'total_price':total_price})
 
 def remove_from_cart(request, item_id):
     """Remove item from the cart"""
