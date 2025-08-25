cebf19486fd851be498e5805adeea4c36f9d45fc
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-25 10:26:01 +0530

    38077

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 
-def order_page(request):
-    breadcrumb = [
-        {'name':'Home', 'url':'/'},
-        {'name':'Order', 'url':'/order/'}
-    ]
-    return render(request, 'order.html',{'breadcrumb':breadcrumb})
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart',{})
+    cart[item_id] = cart.get(item_id, 0) + 1
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
 
 def order_confirmation(request):
     order_id = random.randint(1000, 9999)
