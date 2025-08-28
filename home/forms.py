4a63d700725584335d5483826f4a8300062938aa
MohamedFathil <mohamedfathil057@gmail.com>
2025-08-28 09:31:06 +0530

    39026

--- home/forms.py
+++ home/forms.py
@@ -6,14 +6,19 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name','email','message']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder':'Your Name'}),
+            'email': forms.EmailInput(attrs={'placeholder':'Your Email'}),
+            'message': forms.Textarea(attrs={'placeholder':'Your Message'}),
+        }
     
     def clean_email(self):
-        email = self.cleaned_data('email')
+        email = self.cleaned_data.get('email')
         validate_email(email)
         return email
     
     def clean_message(self):
-        message = self.cleaned_data('message')
+        message = self.cleaned_data.get('message')
         if len(message) < 10:
             raise forms.ValidationError("Message must be at least 10 character long. ")
         return message
