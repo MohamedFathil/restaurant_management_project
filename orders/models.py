198585a1edaf534cc73f9bc34ab439d83b4ef1bf
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-16 16:30:34 +0530

    44883

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import  MenuItem
 
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, help_text='Discount in %')
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
 class OrderStatus(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
