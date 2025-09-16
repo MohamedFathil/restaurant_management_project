7defeefe0306db842558e0075eabebc308967f4b
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-16 16:41:05 +0530

    44882

--- products/models.py
+++ products/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=150, unique = True)
+
+    def __str__(self):
+        return self.name
+
 class MenuItem(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
@@ -7,6 +14,13 @@ class MenuItem(models.Model):
     image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     available = models.BooleanField(default=True)
+    category = models.ForeignKey(
+        MenuCategory,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='items'
+    )
 
     def __str__(self):
         return str(self.name)
