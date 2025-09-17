f11171edc55dea6b312a680d1e67031b6ee2aa01
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-17 13:27:43 +0530

    46578

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .view import OrderHistoryview
 
 urlpatterns = [
     path('add-to-cart/<int:item_id>/',views.add_to_cart, name="add_to_cart"),
     path('cart/', views.view_cart, name="view_cart"),
+    path('api/order-history/', OrderHistoryview.as_view(), name='order_history'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name="remove_from_cart"),
     path('order-confirmation/', views.order_confirmation, name="order_confirmation"),
 ]
