729a547244ac46068077b425ea1eb51c701b563f
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-18 10:19:44 +0530

    47687

--- account/views.py
+++ account/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+
+class UserProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        profile = request.user.profile
+        serializer = UserProfileSerializer(profile, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
