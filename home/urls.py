6338613e29d0a87e04804cef78ed8971dd05ad48
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-23 13:47:16 +0530

    50048

--- home/urls.py
+++ home/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('our-team/', views.our_team, name="our_team"),
     path('gallery/', views.gallery, name="gallery"),
     path('location/', views.location, name="location"),
+    path('api/contact/', views.ContactFormSubmissionView.as_view(), name='contact-form'),
 ]
