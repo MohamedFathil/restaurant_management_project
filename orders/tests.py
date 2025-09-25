8c4e5c966c036bc631f2aa1b504b3433b428b0a8
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-25 14:42:39 +0530

    50689

--- orders/tests.py
+++ orders/tests.py
@@ -23,5 +23,16 @@ class OrderModelTest(TestCase):
         self.assertEqual(total, Decimal("200.00"))
     
     def test_calculate_total_with_coupon(self):
-        coupon = Coupon.objects.create() 
+        coupon = Coupon.objects.create(
+            code="TEST10",
+            discount = Decimal("10.00"),
+            valid_from = timezone.now() - timedelta(days=1),
+            valid_to = timezone.now() + timedelta(days=5),
+            active=True
+        )
+        self.order.coupon = coupon
+        self.order.save()
+
+        total = self.order.calculate_total()
+        self.assertEqual(total, Decimal("225.00"))
     
