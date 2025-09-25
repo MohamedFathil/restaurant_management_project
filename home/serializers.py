3ad859f03b0c1e5f365e5849ddbe37ec58bfc24d
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-25 13:46:41 +0530

    50690

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,14 @@
 from .models import ContactFormSubmission
+from products.models import MenuItem
 from rest_framework import serializers
 
 class ContactFormSubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactFormSubmission
         fields = ['id','name','email','message','submitted_at']
+
+class DailySpecialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id','name','description','price']
 
