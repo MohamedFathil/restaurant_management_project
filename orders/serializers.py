5fa793bdf39203d9607811b745ce92f178a85d5f
MohamedFathil <mohamedfathil057@gmail.com>
2025-09-27 09:09:53 +0530

    50687

--- orders/serializers.py
+++ orders/serializers.py
@@ -25,3 +25,13 @@ class OrderCancelSerializer(serializers.ModelSerializer):
             instance.save()
             return instance
 
+class OrderStatusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id','status']
+
+    def validate_status(self, value):
+        allowed_statuses = [choice[0] for choice in Order.STATUS_CHOICES]
+        if value not in allowed_statuses:
+            raise serializers.ValidationError("Invalid status")
+        return value
